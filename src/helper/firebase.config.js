94a89183746dd0f2edb74c064a7426f6c43f40ee
sajjad-amin <sayem.1998.bd@gmail.com>
2021-04-02 23:22:00 +0600

    .env modified

--- src/helper/firebase.config.js
+++ src/helper/firebase.config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDdNKbW1mKUCecr7pvus_60mgCWzQH5bCw",
-    authDomain: "fresh-bazar-98.firebaseapp.com",
-    projectId: "fresh-bazar-98",
-    storageBucket: "fresh-bazar-98.appspot.com",
-    messagingSenderId: "96765406637",
-    appId: "1:96765406637:web:bcfb804eed45897d624524"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
